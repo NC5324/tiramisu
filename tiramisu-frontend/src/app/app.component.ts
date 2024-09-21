596d73fed57b19390b9f0b3e8700d1cdfda0cec8
Naiddzhan Durmush <naiddzhan.durmush@catenate.com>
2024-09-21 14:56:05 +0300

    feat: enable zoneless; default onpush change detection; add @rx-angular/template;

--- tiramisu-frontend/src/app/app.component.ts
+++ tiramisu-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +7,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'tiramisu';
 }
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+  ],
+});
